d6e98737c20514d37ca223684e4317d05394b27a
escavi <pauline.escavi@gmail.com>
2023-04-08 19:51:47 +0200

    question 1.b

--- exercise1.js
+++ exercise1.js
@@ -10,3 +10,22 @@ function loadDoc() {
     xhttp.open("GET", "text.txt", true);
     xhttp.send();
   }
+
+  function loadDoc2() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        const text = this.responseText.split("\n");
+        const colors = ["red", "green", "blue", "orange", "purple", "yellow"];
+        let html = "";
+        for (let i = 0; i < text.length; i++) {
+          const color = colors[i % colors.length];
+          html += `<p style="color:${color}">${text[i]}</p>`;
+        }
+        document.getElementById("ta2").innerHTML = html;
+      }
+    };
+    xhttp.open("GET", "text.txt", true);
+    xhttp.send();
+  }
+  
