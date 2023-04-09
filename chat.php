98c26ed4f4f0a75da31ccc348a950bbbc29f7956
escavi <pauline.escavi@gmail.com>
2023-04-09 13:05:04 +0200

    test d'écriture sur la console

--- chat.php
+++ chat.php
@@ -3,10 +3,10 @@ $chaine = gethostbyname($_SERVER['REMOTE_ADDR']) ;
 $chaine .=  " - " . $_GET['phrase'] . "\n";
 $fp = fopen("chatlog.txt","a");
 if ($fp == false) {
-  echo "Permission error on chatlog.txt: do 'chmod a+w chatlog.txt'";
+  error("Permission error on chatlog.txt: do 'chmod a+w chatlog.txt'");
 } else {
   fwrite($fp, $chaine);
   fclose($fp);
-  echo "Success";
+  error("Success");
 }
 ?>
